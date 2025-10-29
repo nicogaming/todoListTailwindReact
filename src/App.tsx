03af3799c0409a9b1a68820dd8a777de7317f8b2
Nico_jbm <n.bruunmortensen@gmail.com>
2025-10-29 18:04:19 +0100

    tilføjet todo tilføjelser

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import { useState } from "react"
 import TodoListItem from "./coomponents/todoListItem"
 import { dummyData } from "./data/todo"
+import AddTodoForm from "./coomponents/AddTodoForm"
 
 function App() {
-const[todos, setTodos]= useState(dummyData)
+const[todos, setTodos]= useState(dummyData);
+//opdatere todolisten så man ved vad der er gjort
  function setTodoCompleted(id:number,completed:boolean){
   setTodos((prevTodos)=> prevTodos.map(todo =>(todo.id === id ? {...todo, completed}: todo)))
+ }
+ //tilføjer todos til listen
+ function addTodo(title:string){
+  setTodos(prevTodos => [
+    {
+      id: prevTodos.length + 1,
+      title,
+      completed: false,
+    },
+    ...prevTodos
+  ])
  }
   return (
     <main className="py-10 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center">
         your todos
       </h1>
-      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 ">
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
+        <AddTodoForm
+        onSubmit={addTodo}/>
         <div className="space-y-2">
           {todos.map(todo =>(
             <p key={todo.id} className="text-lg ">
---
+++ src/coomponents/AddTodoForm.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+interface AddTodoForm{
+    onSubmit:(title:string)=> void;
+}
+export default function AddTodoForm({onSubmit}: AddTodoForm){
+    const[input,setInput]= useState("");
+    //handler de nye tasks
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>){
+        e.preventDefault();
+
+        if(!input.trim()) return;
+        onSubmit(input);
+        setInput("");
+    }
+
+    return(
+        <form className="flex" onSubmit={handleSubmit}>
+            <input
+            value={input}
+            onChange={(e)=> setInput(e.target.value)}
+            placeholder="What task do i have"
+            className="rounded-s-md grow border border-gray-400 p-2"
+            />
+            <button type="submit"
+            className="w-16 rounded-e-md bg-slate-900 text-white hover:bg-slate-800"
+            >Add</button>
+        </form>
+    )
+}
